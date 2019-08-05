c830c7d9db811b4c396007cd4ee90a53f9f32ed0
Dennis Pavlik <DJAPavlik@gmail.com>
2019-08-05 10:50:37 -0500

    Working version with the random date button

--- src/app/apod/apod.component.spec.ts
+++ src/app/apod/apod.component.spec.ts
@@ -23,20 +23,6 @@ describe('ApodComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  getApod(date:string): void{
-
-    //If the date is falsy, use today's date
-    if(!date){
-      date = new Date().toISOString().slice(0,10);
-    }
-  
-    this.apodService.getApod(date).subscribe(
-      (response:any)=>{
-        this.apod = response;
-        this.date = this.randomDate(new Date(1995,5,16), new Date());
-        console.log(this.apod);
-      }
-    );
 
 
 
--- src/app/apod/apod.component.ts
+++ src/app/apod/apod.component.ts
@@ -26,37 +26,18 @@ export class ApodComponent implements OnInit {
     });
   }
 
-//4. Replace the current date with an updated method signature
-getApod(date:string): void{
+  getApod(date:string): void{
 
-  this.apodService.getApod(date).subscribe(
-    (response:any)=>{
-      this.apod = response;
-
-      //5. Log the results to the JS console
-      console.log(response);
-    }
-  );
-
-  }
-}
-
-/*  getApod(date:string): void{
-
-    //If the date is falsy, use today's date
-    if(!date){
-      date = new Date().toISOString().slice(0,10);
-    } 
-    
- 
     this.apodService.getApod(date).subscribe(
       (response:any)=>{
         this.apod = response;
+        //3.  Update this.date on each API call
         this.date = this.randomDate(new Date(1995,5,16), new Date());
-        console.log(this.apod);
+        //4. Log this.date to the JS console
+        console.log(this.date);
       }
     );
-  
+
   }
 
   //2. Create a method that returns a random date
@@ -69,4 +50,10 @@ getApod(date:string): void{
     return new Date(date).toISOString().slice(0,10);
   }
 
-} */
+
+
+  
+  
+
+} // end ApodComponent
+
